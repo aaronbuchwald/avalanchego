7c1a7d8a6891495d0a12b60e734eb2948b7cd0bf
Aaron Buchwald <aaron.buchwald56@gmail.com>
2024-02-06 16:17:07 -0500

    add expected blue math helper wo byzantine calculations

---
+++ snow/consensus/snowball/math_helpers.go
@@ -0,0 +1,64 @@
+// Copyright (C) 2019-2024, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package snowball
+
+import "math/big"
+
+const precision = 1024
+
+// P(Sum(X_{1-k}) >= alpha | P(X_i = 1) = p ^ P(X_i = 0) = 1-p)
+func binomial(p *big.Float, k, alpha int) *big.Float {
+	bigNotP := big.NewFloat(1).SetPrec(precision)
+	bigNotP.Sub(bigNotP, p)
+
+	var (
+		probability              = big.NewFloat(0).SetPrec(precision)
+		pToAlpha                 = big.NewFloat(1).SetPrec(precision)
+		notPToNotAlpha           = big.NewFloat(1).SetPrec(precision)
+		binomialCoefficientInt   = new(big.Int)
+		binomialCoefficientFloat = new(big.Float).SetPrec(precision)
+	)
+	for i := 0; i < alpha; i++ {
+		pToAlpha.Mul(pToAlpha, p)
+	}
+	for i := alpha; i < k; i++ {
+		notPToNotAlpha.Mul(notPToNotAlpha, bigNotP)
+	}
+	for ; alpha <= k; alpha++ {
+		binomialCoefficientFloat.SetInt(binomialCoefficientInt.Binomial(int64(k), int64(alpha))) // k choose alpha
+		binomialCoefficientFloat.Mul(binomialCoefficientFloat, pToAlpha).Mul(binomialCoefficientFloat, notPToNotAlpha)
+		probability.Add(probability, binomialCoefficientFloat)
+
+		pToAlpha.Mul(pToAlpha, p)
+		notPToNotAlpha.Quo(notPToNotAlpha, bigNotP)
+	}
+	return probability
+}
+
+// calculateExpectedBluePortion calculates the expected portion of nodes that will be blue in the next round
+// given all nodes follow the protocol with the given parameters.
+func calculateExpectedBluePortion(portionBlue, portionByzantine *big.Float, k, alpha int) *big.Float {
+	portionRed := big.NewFloat(1)
+	portionRed.Sub(portionRed, portionBlue)
+
+	prFlipRed := binomial(portionRed, k, alpha)
+	expectedPortionFlipRed := new(big.Float).Mul(portionBlue, prFlipRed)
+	expectedPortionStayBlue := new(big.Float).Sub(portionBlue, expectedPortionFlipRed)
+
+	prFlipBlue := binomial(portionBlue, k, alpha)
+	expectedPortionFlipBlue := new(big.Float).Mul(portionRed, prFlipBlue)
+
+	return new(big.Float).Add(expectedPortionStayBlue, expectedPortionFlipBlue)
+}
+
+func precalculateExpectedBluePortion(start, end, interval, portionByzantine *big.Float, k, alpha int) map[float64]float64 {
+	res := make(map[float64]float64)
+	for portionBlue := start; portionBlue.Cmp(end) <= 0; portionBlue.Add(portionBlue, interval) {
+		portionBlueFloat, _ := portionBlue.Float64()
+		expectedPortionBlue, _ := calculateExpectedBluePortion(portionBlue, portionByzantine, k, alpha).Float64()
+		res[portionBlueFloat] = expectedPortionBlue
+	}
+
+	return res
+}
---
+++ snow/consensus/snowball/math_helpers_test.go
@@ -0,0 +1,18 @@
+// Copyright (C) 2019-2024, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package snowball
+
+import (
+	"math/big"
+	"testing"
+)
+
+func TestPrecalculateNextPortionBlue(t *testing.T) {
+	// given a specific portion of stake that is actually blue/red, I want to set preferences of the byzantine nodes
+	// to reflect as close a s possible to a portion that 
+	// map current blue -> expected blue
+	// given there are b portion byzantine nodes, and we know the preferences of 
+	res := precalculateExpectedBluePortion(big.NewFloat(0.1), big.NewFloat(0.9), big.NewFloat(0.001), 20, 11)
+	t.Fatal(res)
+}
