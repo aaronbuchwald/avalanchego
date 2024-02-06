eb4f33dd95fbd181ea4d673947edccfc866c120b
Aaron Buchwald <aaron.buchwald56@gmail.com>
2024-02-06 16:18:00 -0500

    add 0 param to test

--- snow/consensus/snowball/math_helpers_test.go
+++ snow/consensus/snowball/math_helpers_test.go
@@ -10,9 +10,9 @@ import (
 
 func TestPrecalculateNextPortionBlue(t *testing.T) {
 	// given a specific portion of stake that is actually blue/red, I want to set preferences of the byzantine nodes
-	// to reflect as close a s possible to a portion that 
+	// to reflect as close a s possible to a portion that
 	// map current blue -> expected blue
-	// given there are b portion byzantine nodes, and we know the preferences of 
-	res := precalculateExpectedBluePortion(big.NewFloat(0.1), big.NewFloat(0.9), big.NewFloat(0.001), 20, 11)
+	// given there are b portion byzantine nodes, and we know the preferences of
+	res := precalculateExpectedBluePortion(big.NewFloat(0.1), big.NewFloat(0.9), big.NewFloat(0.001), big.NewFloat(0), 20, 11)
 	t.Fatal(res)
 }
