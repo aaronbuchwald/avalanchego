3e0ede9eae2a9a4e2d8254c13087439459432c0f
Aaron Buchwald <aaron.buchwald56@gmail.com>
2024-02-19 17:07:37 -0500

    Update graph to create violin/boxplot overlaid with scatterplot

--- cmd/snowball/graph.py
+++ cmd/snowball/graph.py
@@ -1,19 +1,38 @@
-import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
 
-# Read the CSV file into a pandas DataFrame
-data = pd.read_csv('snow-sim-output-k-80.csv')
 
-# Create a scatter plot
-plt.scatter(data['byz'], data['rounds'], alpha=0.5)
+def graph_byz_round_to_termination(csv: str):
+    sns.set_theme(style="ticks") # sets a global theme
 
-# Add colorbar to accentuate data concentration
-plt.colorbar()
+    # Initialize the figure with a linear x axis
+    f, ax = plt.subplots(figsize=(7, 6))
+    ax.set_xscale("linear")
 
-# Set labels and title
-plt.xlabel('byz')
-plt.ylabel('rounds')
-plt.title('Scatter Plot')
+    # Load the sim output data
+    simdata = pd.read_csv(csv)
 
-# Show the plot
-plt.show()
+    # Plot the portion of byzantine stake vs. rounds to termination
+    # capped at 1k rounds
+    # violin plot may be good too
+    sns.violinplot(
+        simdata, x="byz", y="rounds", hue="byz", width=.4, palette="vlag",
+        # whis=[0, 100],
+    )
+
+    # Add in points to show each observation
+    sns.stripplot(simdata, x="byz", y="rounds", size=1, color=".4")
+
+    # Tweak the visual presentation
+    ax.xaxis.grid(True)
+    ax.set(ylabel="")
+    sns.despine(trim=True, left=True)
+
+    plt.show()
+
+def run():
+    graph_byz_round_to_termination('snow-sim-output-k-80.csv')
+
+if __name__ == "__main__":
+    run()
