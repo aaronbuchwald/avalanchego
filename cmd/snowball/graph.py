ecadab4bf80a97820b617fc2356688e41d59b47d
Aaron Buchwald <aaron.buchwald56@gmail.com>
2024-02-27 18:10:39 -0500

    Update fontsize to look better within paper

--- cmd/snowball/graph.py
+++ cmd/snowball/graph.py
@@ -6,6 +6,8 @@ import pandas as pd
 def graph_byz_round_to_termination(csv: str, n: int, k: int, save_path: str = None):
     sns.set_theme(style="ticks") # sets a global theme
 
+    fontsize = 24
+
     # Initialize the figure with a linear x axis
     f, ax = plt.subplots(figsize=(10, 8))
     ax.set_xscale("linear")
@@ -17,30 +19,37 @@ def graph_byz_round_to_termination(csv: str, n: int, k: int, save_path: str = No
     # capped at 1k rounds
     # violin plot may be good too
     sns.violinplot(
-        simdata, x="byz", y="rounds", hue="byz", width=.4, palette="vlag",
-        # whis=[0, 100],
-    ).set_title('Byzantine Stake vs. Rounds to Termination')
+        simdata, x="byz", y="rounds", hue="byz", width=.2, palette=sns.color_palette("ch:s=.25,rot=-.25", as_cmap=True),
+        # whis=[0.25, 0.75],
+    ).set_title(f"n = {'{:,}'.format(n)}", fontsize=fontsize)
 
     # Add in points to show each observation
     sns.stripplot(simdata, x="byz", y="rounds", size=1, color=".4")
 
     # Tweak the visual presentation
     ax.xaxis.grid(True)
-    ax.set(ylabel="")
+    ax.set_xlabel("Byzantine Stake", fontsize=fontsize)
+    ax.set_ylabel("Rounds to Termination", fontsize=fontsize)
     sns.despine(trim=True, left=True)
 
-    plt.figtext(0.9, 0.05, f'n = {n}\nk = {k}', ha='center', va='center', fontsize=10, bbox=dict(facecolor='white', alpha=0.5, edgecolor='black'))
-
+    plt.xticks(fontsize=fontsize)
+    plt.yticks(fontsize=fontsize)
 
     if save_path is not None:
         plt.savefig(save_path)
+    else:
+        plt.show()
 
-    plt.show()
-
-def run():
-    n, k = 10000, 80
+def show():
+    n, k = 500, 80
     base = f'snow-sim/snow-sim-output-n-{n}-k-{k}'
-    graph_byz_round_to_termination(f'{base}.csv', n, k, f'{base}.png')
+    graph_byz_round_to_termination(f'{base}.csv', n, k)
+
+def save_each():
+    k = 80
+    for n in [500, 1000, 10000]:
+        base = f'snow-sim/snow-sim-output-n-{n}-k-{k}'
+        graph_byz_round_to_termination(f'{base}.csv', n, k, f'{base}.png')
 
 if __name__ == "__main__":
-    run()
+    show()
