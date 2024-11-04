6d6c150a5376fab32c3111a731c51e16f6a03032
Aaron Buchwald <aaron.buchwald56@gmail.com>
2024-11-03 21:25:25 -0500

    Add address to original child type

--- x/merkledb/node.go
+++ x/merkledb/node.go
@@ -20,6 +20,7 @@ type child struct {
 	compressedKey Key
 	id            ids.ID
 	hasValue      bool
+	address       diskAddress
 }
 
 // node holds additional information on top of the dbNode that makes calculations easier to do
